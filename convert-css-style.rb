db5aeda5320b2232011b81e9532a1897f48bec7e
Hashimoto Yuya <hy.customlife@gmail.com>
2023-10-25 17:26:04 +0900

    Fix convert css style script to make new css.ts file

--- convert-css-style.rb
+++ convert-css-style.rb
@@ -1,42 +1,52 @@
 #!/usr/bin/ruby
 
-# Replace linaria styled`` to vanilla-extract style({})
+# Convert linaria css style to vanilla extract css style
 
+full_filepath = ARGV[0]
+filename = full_filepath.split("/")[-1]
+new_filename = filename.sub("tsx", "css.ts")
+filepath = full_filepath.split("/")[0..-2].join("/")
+new_fullpath = "#{filepath}/#{new_filename}"
+
+text = ""
 File.open(ARGV[0]) do |file|
     text = file.read
+end
 
-    regex = %r|const (\w+) = styled.*?`(.*?)`|m
-    name_css_hash = {}
-    css_variables = []
-    text.scan(regex).each do |name, css|
-        list = css.split(/;/).map do |s|
-            pair = s.split(/:/)
-            key = pair[0].gsub(%r|-([a-z])|) { $1.upcase }
-            value = pair[1].gsub(%r|^(\s*)(.*?)(\s*)$|, '\1"\2"\3') if pair[1]
-            regex = %r|\"\$\{(.*)\}\"|
-            if regex.match(value)
-                var = value.gsub(regex, '\1')
-                css_variables.append(var.strip)
-                "#{key}:#{var}"
-            else
-                "#{key}:#{value}"
-            end
+regex = %r|const (\w+) = styled.*?`(.*?)`|m
+name_css_hash = {}
+css_variables = []
+text.scan(regex).each do |name, css|
+    list = css.split(/;/).map do |s|
+        pair = s.split(/:/)
+        key = pair[0].gsub(%r|-([a-z])|) { $1.upcase }
+        value = pair[1].gsub(%r|^(\s*)(.*?)(\s*)$|, '\1"\2"\3') if pair[1]
+        regex = %r|\"\$\{(.*)\}\"|
+        if regex.match(value)
+            var = value.gsub(regex, '\1')
+            css_variables.append(var.strip)
+            "#{key}:#{var}"
+        else
+            "#{key}:#{value}"
         end
-
-        name = name.gsub(/^Styled/, '')
-        new_name = name[0].downcase + name[1..-1]
-        name_css_hash[new_name] = "export const #{new_name}Style = style({" + list.join(",") + "})"
     end
 
-    puts %q(import {style} from "@vanilla-extract/css";)
+    name = name.gsub(/^Styled/, '')
+    new_name = name[0].downcase + name[1..-1]
+    name_css_hash[new_name] = "export const #{new_name}Style = style({" + list.join(",") + "})"
+end
+
+File.open(new_fullpath, "w") do |io|
+    io.write(%q(import {style} from "@vanilla-extract/css";))
+    io.write("\n")
 
     css_variables.each do |var|
-      path = text.scan(%r|import \{.*#{var}.*\} from \"(.*)\";|)[0][0]
-      puts %Q(import {#{var}} from "#{path}";)
+        path = text.scan(%r|import \{.*#{var}.*\} from \"(.*)\";|)[0][0]
+        io.write(%Q(import {#{var}} from "#{path}";))
     end
 
     name_css_hash.each do |_, value|
-        puts
-        puts value + ";"
+        io.write("\n\n")
+        io.write("#{value};")
     end
 end
