1a3e19274b3eb8f8adc2163b8a404ceff09cb692
Hashimoto Yuya <hy.customlife@gmail.com>
2023-10-25 17:36:05 +0900

    Fix convert react tag script to overwrite tsx file

--- convert-react-tag.rb
+++ convert-react-tag.rb
@@ -8,31 +8,35 @@ new_filename = filename.gsub("tsx", "css")
 
 linaria_style_regex = %r|const (\w+) = styled.(.*?)`.*?`;|m
 
+text = ""
 File.open(ARGV[0]) do |file|
     text = file.read
+end
 
-    list = text.scan(linaria_style_regex)
-    new_name_list = []
-    list.each do |name, tag|
-        name = name.gsub(/^Styled/, '')
-        new_name = name[0].downcase + name[1..-1] + "Style"
-        new_name_list.append(new_name)
+list = text.scan(linaria_style_regex)
+new_name_list = []
+list.each do |name, tag|
+    name = name.gsub(/^Styled/, '')
+    new_name = name[0].downcase + name[1..-1] + "Style"
+    new_name_list.append(new_name)
 
-        regex = %r|<#{name}(.*)<\/#{name}>|m
+    regex = %r|<#{name}(.*)<\/#{name}>|m
 
-        if text.match(regex)
-            text.gsub!(regex) do
-                "<#{tag} className={#{new_name}}#{$1}</#{tag}>"
-            end
-        else
-            text.gsub!(%r|<#{name}(.*?)\/>|m) do
-                "<#{tag} className={#{new_name}}#{$1}/>"
-            end
+    if text.match(regex)
+        text.gsub!(regex) do
+            "<#{tag} className={#{new_name}}#{$1}</#{tag}>"
+        end
+    else
+        text.gsub!(%r|<#{name}(.*?)\/>|m) do
+            "<#{tag} className={#{new_name}}#{$1}/>"
         end
     end
+end
 
-    puts %Q(import {#{new_name_list.join(", ")}} from "#{new_filename}")
-    puts text
+File.open(ARGV[0], 'w') do |io|
+    io.write(%Q(import {#{new_name_list.join(", ")}} from "#{new_filename}";\n))
+    io.write(text
       .gsub(linaria_style_regex, '')
       .gsub(%r|\n{3,}|) { "\n\n" }
+    )
 end
