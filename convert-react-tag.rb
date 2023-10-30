db913e245299c8dccc89a588eeeb7f947eb2c44c
Hashimoto Yuya <hy.customlife@gmail.com>
2023-10-30 17:26:48 +0900

    Fine tune about react tag script

--- convert-react-tag.rb
+++ convert-react-tag.rb
@@ -34,7 +34,9 @@ list.each do |name, tag|
 end
 
 File.open(ARGV[0], 'w') do |io|
-    io.write(%Q(import {#{new_name_list.join(", ")}} from "./#{new_filename}";\n))
+    if new_name_list.length > 0
+        io.write(%Q(import {#{new_name_list.join(", ")}} from "./#{new_filename}";\n))
+    end
     io.write(text
       .gsub(linaria_style_regex, '')
       .gsub(%r|\n{3,}|) { "\n\n" }
