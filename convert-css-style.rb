47c3d6762de129379af3955d0cd3a01244bc4873
Hashimoto Yuya <hy.customlife@gmail.com>
2023-11-01 15:07:48 +0900

    Remove css variables from convert css style script

--- convert-css-style.rb
+++ convert-css-style.rb
@@ -3,33 +3,29 @@
 # Convert linaria css style to vanilla extract css style
 
 def process_css(css)
-    css_variables = []
     css_regex = %r%(.*?):(.*?);%m
-    list = css.scan(css_regex).map do |key, value|
+    css.scan(css_regex).map do |key, value|
         kebab_key = key.gsub(%r|-([a-z])|) { $1.upcase }
         quoted_value = value.gsub(%r|^(\s*)(.*?)(\s*)$|, '\1"\2"\3') if value
+        // Can refactor
         variable_regex = %r|\"\$\{(.*)\}\"|
         if variable_regex.match(quoted_value)
-            var = quoted_value.gsub(variable_regex, '\1')
-            css_variables.append(var.strip)
-            "#{kebab_key}:#{var},"
+            "#{kebab_key}:#{quoted_value.gsub(variable_regex, '\1')},"
         else
             "#{kebab_key}:#{quoted_value},"
         end
     end
-    [list, css_variables]
 end
 
 def convert_media_query(css)
     words_regex = Regexp.union(["mediaUpToSmall", "mediaMiddleUp"])
-    regex = %r|\$\{(#{words_regex})\} \{(.*?)\}|m
-    css.gsub(regex) do
+    css.gsub(%r|\$\{(#{words_regex})\} \{(.*?)\}|m) do
         word = $1
         contents = $2
         new_word = "vanillaExtract" + word[0].upcase + word[1..-1]
         <<TEXT
 "@media": {
-        [#{new_word.strip}]: {
+        [#{new_word}]: {
             #{contents.strip}
         },
     },
@@ -49,9 +45,8 @@ File.open(ARGV[0]) do |file|
 end
 
 name_css_hash = {}
-css_variables = []
 text.scan(%r|const (\w+) = styled.*?`(.*?)`|m).each do |name, css|
-    list, css_variables = process_css(css)
+    list = process_css(css)
 
     name = name.gsub(/^Styled/, '')
     new_name = name[0].downcase + name[1..-1]
@@ -62,14 +57,7 @@ File.open(new_fullpath, "w") do |io|
     io.write(%q(import {style} from "@vanilla-extract/css";))
     io.write("\n")
 
-    css_variables.each do |var|
-        pathList = text.scan(%r|import \{.*#{var}.*\} from \"(.*?)\";|m)
-        unless pathList.empty?
-            path = pathList[0][0]
-            io.write(%Q(import {#{var}} from "#{path}";\n))
-        end
-    end
-
+    // Can refacotr
     name_css_hash.each do |_, value|
         io.write("\n\n")
         io.write("#{convert_media_query(value)}")
